545622a78d7448766f9671de3dddd90d2281ce5d
marulog <gr214777@naver.com>
2025-12-01 21:11:29 +0900

    chore: initial project steup

---
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,41 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    // 연관관계 편의 메소드
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
